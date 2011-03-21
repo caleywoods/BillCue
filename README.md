cd95b2a178c94265abb392cbe449f7e4023da84e
Caley Woods <caley.w@gmail.com>
2011-03-21 14:14:04 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+BillCue
+=======
+
+BillCue is a calendar app done in rails 3. It uses devise for
+authentication, devise_invitable to allow invitations, haml for
+templating, and rspec/autotest for unit tests.
+
+It will first support email reminders but should quickly integrate
+sms reminders using Twilio and then expand again to support tweets and
+DM's.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Billcue::Application.config.secret_token = '7e32b8233df3f7a7fa933a3b59f1412aeaeb4ec055ae5909ba90801d2ba9e8cd14c79ac64b10396fd6b8878d3394cd4dff85bae3b2584b086da52e3ffac4b6bd'
