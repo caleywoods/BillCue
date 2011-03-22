f7f7eef37174f881facf9f72a9395ae111a88ba3
Caley Woods <caley.w@gmail.com>
2011-03-21 23:24:57 -0500

    Added create method to allow event/bill creation. Form more complete for a new event. Modal dialog for new event now fully functional

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,6 +8,14 @@ class EventsController < ApplicationController
   end 
 
   def create
+    @event = Event.new(params[:event])
 
+    respond_to do |format|
+      if @event.save!
+        format.html { redirect_to(root_path, :notice => 'Your event was successfully created.') }
+      else
+        format.html { render :action => "new" }
+      end
+    end
   end
 end
