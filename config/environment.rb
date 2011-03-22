020debeee48d6481811455b10336d8e622739247
Caley Woods <caley.w@gmail.com>
2011-03-22 11:20:49 -0500

    Changed show template from using Time.strftime to a custom format defined in environment.rb. DRY FTW

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Billcue::Application.initialize!
+
+# Add our own custom date format
+Time::DATE_FORMATS[:due] = 'due on %m/%d'
